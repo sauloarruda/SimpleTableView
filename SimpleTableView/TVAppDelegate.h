b9ee0909f7fd10c98680a905ca5299efad2cef14
Saulo Arruda <sauloarruda@gmail.com>
2012-09-18 21:11:16 -0400

    Initial Commit

---
+++ SimpleTableView/TVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TVAppDelegate.h
+//  SimpleTableView
+//
+//  Created by Saulo Arruda on 9/18/12.
+//  Copyright (c) 2012 Jera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleTableView
+//
+//  Created by Saulo Arruda on 9/18/12.
+//  Copyright (c) 2012 Jera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TVAppDelegate class]));
+    }
+}
